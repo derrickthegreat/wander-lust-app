bd0bda7ee590c9e2118b8e25d9a06d5ffd93c6f1
Derrick A <derrickcanbereached@gmail.com>
2021-07-05 07:50:43 -0400

    add getForecast & getVenues functions

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,7 @@ const appId = `&appid=` + OW_KEY;
 
 // DOM Mapping
 const inputField = document.getElementById('city');
-const submitButton = document.getElementById('button');
+const submitBtn = document.getElementById('button');
 const destination = document.getElementById('destination');
 const container = document.getElementsByClassName('container');
 const venue1 = document.getElementById('venue1');
@@ -21,4 +21,55 @@ const venueDivs = [venue1, venue2, venue3];
 const weatherDiv = document.getElementById('weather1');
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+submitBtn.addEventListener('click', executeSearch, false);
+
+
+
 // AJAX Functions
+async function getVenues() {
+    const city = inputField.value;
+    const urlToFetch = url + city + `&limit=5` + `&v=20210705`;
+    try {
+        const response = await fetch(urlToFetch);
+        if(response.ok) {
+            const jsonResponse = await response.json();
+            const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+            // console.log(venues);
+            return venues;
+        }
+        throw new Error('Unable to pull venues.');
+    } catch(err) {
+        console.log(err);
+    };
+};
+
+async function getForecast() {
+    const city = inputField.value;
+    const urlToFetch = weatherURL + city + `&units=imperial` + appId ;
+    try {
+        const response = await fetch(urlToFetch);
+        if(response.ok) {
+            const jsonResponse = await response.json();
+            /*
+            jsonResponse.name - City Name
+            jsonResponse.main
+                    ""       .feels_like
+                    ""       .temp
+                    ""       .temp_min
+                    ""       .temp_max
+            jsonResponse.weather - Weather Conditions (raining, cloudy)
+            */
+            return jsonResponse
+        }
+        throw new Error('Unable to pull weather.');
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+function executeSearch(e) {
+    e.preventDefault();
+    //clear dom elements
+    getForecast();
+    getVenues();
+};
