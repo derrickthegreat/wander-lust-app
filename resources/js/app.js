87a36435a8f214a0ac710300dd193c85cfed3d24
Derrick A <derrickcanbereached@gmail.com>
2021-07-05 06:50:37 -0400

    create input form for search

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,15 @@
-import { FS_KEY } from './keys.js';
+import { FS_KEY, FS_SECRET, OW_KEY } from './keys.js';
+
+// FourSquare Variables
+const clientId = FS_KEY;
+const clientSecret = FS_SECRET;
+const url = `https://api.foursquare.com/v2/venues/explore?client_id=` + clientId + `&client_secret=` + clientSecret;
+// FS Query Params:
+const nearParam = `&near=`;
+
+// OpenWeather Variables
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const appId = `&appid=` + OW_KEY;
+
+// DOM Mapping
 
